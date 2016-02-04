66e17e3aa1e1d57df0a012953414c2fc88641fa3
justitow <justitow@gmail.com>
2016-02-04 14:06:11 -0800

    again, maybe fixed it

--- decoder.cpp
+++ decoder.cpp
@@ -61,8 +61,7 @@ void Decoder::movl()
 }  // movl()
 
 
-void Decoder::parse(Instruction *instruction, Registers *registers,
-			int memory[1001])
+void Decoder::parse(Instruction *instruction, Registers *registers, int memory[1001])
 {
   char *ptr, info[1000];
   
